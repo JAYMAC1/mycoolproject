9c8cfea7914f3617f4528f17e8fff59035e7b452
jaymac1 <--replace-all>
2020-10-04 15:24:22 +0100

    added comments to script

--- computer_audit.ps1
+++ computer_audit.ps1
@@ -1,5 +1,9 @@
-
+# Takes in computer to be audited as an argument for the script 
+# c:\projects\computer_audit.ps1 <ComputerName>
 $computerName = $args[0]
+
+# Get disk info using wmi object class Win32_LogicalDisk
+# Tip: to find classes available do a search as shown here PS C:> Get-WmiObject -List *disk* 
 $disks = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computerName
 
 Write-Host "$($computerName)"
